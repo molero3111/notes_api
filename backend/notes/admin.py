926cf3ba05bf74f155c33f8ff25e760b9d7e68cd
molero3111 <molero3111@gmail.com>
2024-05-31 12:25:04 -0400

    Deleted backend folder

--- backend/notes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from notes.models import Category, Tag, Note
-
-# Register your models here.
-admin.site.register(Tag)
-admin.site.register(Category)
-admin.site.register(Note)
